8dd05d138a482a9f3a6e432c4fd0d4471dd379fd
Gwyn <gwyn@playtechnique.io>
2021-03-02 06:07:45 -0700

    Make the purpose of the vpc clearer with renames towards jenkins as a standard identifier.

--- jenkins_network.tf
+++ jenkins_network.tf
@@ -21,11 +21,11 @@ resource "aws_subnet" "jenkins_subnet" {
   cidr_block = cidrsubnet(aws_vpc.jenkins_vpc.cidr_block, 6, 0)
 }
 
-resource "aws_internet_gateway" "cyan_igw" {
+resource "aws_internet_gateway" "jenkins_igw" {
   vpc_id = aws_vpc.jenkins_vpc.id
 
   tags = {
-    Name = "bluey_igw"
+    Name = "jenkins_igw"
   }
 }
 
@@ -33,12 +33,12 @@ output "inspect_subnet" {
   value = aws_subnet.jenkins_subnet.cidr_block
 }
 
-resource "aws_route_table" "cyan_routes" {
+resource "aws_route_table" "jenkins_routes" {
   vpc_id = aws_vpc.jenkins_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.cyan_igw.id
+    gateway_id = aws_internet_gateway.jenkins_igw.id
   }
 
   tags = {
@@ -48,5 +48,5 @@ resource "aws_route_table" "cyan_routes" {
 
 resource "aws_route_table_association" "aws_access" {
   subnet_id      = aws_subnet.jenkins_subnet.id
-  route_table_id = aws_route_table.cyan_routes.id
+  route_table_id = aws_route_table.jenkins_routes.id
 }
